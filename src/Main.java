33021ea7eea821e6da2d905f82555ec0a1db9246
Tatyana Fomina <tatyana24fomina@gmail.com>
2021-06-23 20:04:17 +0400

    changes

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,17 @@ public class Main {
         int minAmount = 100;
         int everyAmountSpent = 50;
         int bonusInRubbles = 1;
-        int minDepositAmount = 500;
 
         int payment = 800;
+        int bonus = payment / everyAmountSpent * bonusInRubbles;
+        System.out.println(bonus);
 
         int totalAmount;
-        if (minDepositAmount >= 500) {
+        if (payment > 500) {
             totalAmount = minAmount + payment + payment / everyAmountSpent * bonusInRubbles;
         } else {
             totalAmount = minAmount + payment;
         }
         System.out.println(totalAmount);
-
-        int totalBonus;
-        if (minDepositAmount >= 500) {
-            totalBonus = payment / everyAmountSpent * bonusInRubbles;
-        } else {
-            totalBonus = 0;
-        }
-        System.out.println(totalBonus);
     }
 }
